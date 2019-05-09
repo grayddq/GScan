d1aca096ff0a07e7b645d24f443ccabcf09375bb
grayddq <grayddq@gmail.com>
2019-05-09 23:47:37 +0800

    code_bug

--- lib/core/data_aggregation.py
+++ lib/core/data_aggregation.py
@@ -25,39 +25,42 @@ class Data_Aggregation:
         except:
             return 1
 
+    def agregation(self):
+        say_info, i = u'-' * 30 + u'\n', 1
+        say_info += u'根据系统分析的情况，溯源后的行动轨迹为：\n'
+        self.result_infos.sort(cmp=self.cmp_datetime, key=operator.itemgetter(u'异常时间'))
+        for result_info in self.result_infos:
+            if result_info[u'检测项'] == u'常规后门检测':
+                say_info += u"[%d] 黑客在%s时间，进行了%s植入,%s \n" % (
+                    i, result_info[u'异常时间'], result_info[u'风险名称'], result_info[u'异常信息'])
+            if result_info[u'检测项'] == u'配置类安全检测':
+                say_info += u"[%d] 黑客在%s时间，进行了%s变更，%s\n" % (
+                    i, result_info[u'异常时间'], result_info[u'风险名称'], result_info[u'异常信息'])
+            if result_info[u'检测项'] == u'文件类安全检测':
+                say_info += u"[%d] 黑客在%s时间，植入了恶意文件%s，%s\n" % (
+                    i, result_info[u'异常时间'], result_info[u'异常文件'], result_info[u'异常信息'])
+            if result_info[u'检测项'] == u'主机历史操作类安全检测':
+                say_info += u"[%d] 黑客在%s时间，进行了恶意操作，%s\n" % (i, result_info[u'异常时间'], result_info[u'异常信息'])
+            if result_info[u'检测项'] == u'日志类安全检测':
+                say_info += u"[%d] 黑客在%s时间，进行了主机登陆，%s\n" % (i, result_info[u'异常时间'], result_info[u'异常信息'])
+            if result_info[u'检测项'] == u'网络链接类安全检测':
+                say_info += u"[%d] 黑客在%s时间，%s\n" % (i, result_info[u'异常时间'], result_info[u'异常信息'])
+            if result_info[u'检测项'] == u'进程类安全检测':
+                say_info += u"[%d] 黑客在%s时间，启动进程%s，%s\n" % (
+                    i, result_info[u'异常时间'], result_info[u'进程PID'], result_info[u'异常信息'])
+            if result_info[u'检测项'] == u'Rootkit类安全检测':
+                say_info += u"[%d] 黑客在%s时间，植入Rootkit后门，%s\n" % (i, result_info[u'异常时间'], result_info[u'异常信息'])
+            if result_info[u'检测项'] == u'系统初始化检测':
+                say_info += u"[%d] 黑客在%s时间，设置了系统命令别名，%s\n" % (i, result_info[u'异常时间'], result_info[u'异常信息'])
+            if result_info[u'检测项'] == u'账户类安全检测':
+                say_info += u"[%d] 黑客在%s时间，进行了账户修改设置，%s\n" % (i, result_info[u'异常时间'], result_info[u'异常信息'])
+            if result_info[u'检测项'] == u'Webshell安全检测':
+                say_info += u"[%d] 黑客在%s时间，植入了webshell文件%s\n" % (i, result_info[u'异常时间'], result_info[u'异常文件'])
+            i += 1
+        file_write(say_info)
+        print(say_info)
+
     def run(self):
         self.result_infos = get_value('RESULT_INFO')
         self.result_infos = reRepeat(self.result_infos)
-        print(u'-' * 30)
-        say_info = u'根据系统分析的情况，溯源后的行动轨迹为：'
-        print(u'\033[1;31m根据系统分析的情况，溯源后的行动轨迹为：\033[0m')
-        self.result_infos.sort(cmp=self.cmp_datetime, key=operator.itemgetter(u'异常时间'))
-        i = 1
-        for result_info in self.result_infos:
-            if result_info[u'检测项'] == u'常规后门检测':
-                print(
-                    u"[%d] 黑客在%s时间，进行了%s植入,%s" % (i, result_info[u'异常时间'], result_info[u'风险名称'], result_info[u'异常信息']))
-            if result_info[u'检测项'] == u'配置类安全检测':
-                print(u"[%d] 黑客在%s时间，进行了%s变更，%s" % (
-                    i, result_info[u'异常时间'], result_info[u'风险名称'], result_info[u'异常信息']))
-            if result_info[u'检测项'] == u'文件类安全检测':
-                print(u"[%d] 黑客在%s时间，植入了恶意文件%s，%s" % (
-                    i, result_info[u'异常时间'], result_info[u'异常文件'], result_info[u'异常信息']))
-            if result_info[u'检测项'] == u'主机历史操作类安全检测':
-                print(u"[%d] 黑客在%s时间，进行了恶意操作，%s" % (i, result_info[u'异常时间'], result_info[u'异常信息']))
-            if result_info[u'检测项'] == u'日志类安全检测':
-                print(u"[%d] 黑客在%s时间，进行了主机登陆，%s" % (i, result_info[u'异常时间'], result_info[u'异常信息']))
-            if result_info[u'检测项'] == u'网络链接类安全检测':
-                print(u"[%d] 黑客在%s时间，%s" % (i, result_info[u'异常时间'], result_info[u'异常信息']))
-            if result_info[u'检测项'] == u'进程类安全检测':
-                print(
-                    u"[%d] 黑客在%s时间，启动进程%s，%s" % (i, result_info[u'异常时间'], result_info[u'进程PID'], result_info[u'异常信息']))
-            if result_info[u'检测项'] == u'Rootkit类安全检测':
-                print(u"[%d] 黑客在%s时间，植入Rootkit后门，%s" % (i, result_info[u'异常时间'], result_info[u'异常信息']))
-            if result_info[u'检测项'] == u'系统初始化检测':
-                print(u"[%d] 黑客在%s时间，设置了系统命令别名，%s" % (i, result_info[u'异常时间'], result_info[u'异常信息']))
-            if result_info[u'检测项'] == u'账户类安全检测':
-                print(u"[%d] 黑客在%s时间，进行了账户修改设置，%s" % (i, result_info[u'异常时间'], result_info[u'异常信息']))
-            if result_info[u'检测项'] == u'Webshell安全检测':
-                print(u"[%d] 黑客在%s时间，植入了webshell文件%s" % (i, result_info[u'异常时间'], result_info[u'异常文件']))
-            i += 1
+        self.agregation()
